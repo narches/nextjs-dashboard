b7654736262b83d99a9c3e3096bef7d6b9a7c95d
JoE <156410520+narches@users.noreply.github.com>
2024-12-07 06:35:55 +0100

    Sidenav

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -18,7 +18,11 @@ export default function SideNav() {
       <div className="flex flex-row md:flex-col justify-between space-x-2 md:space-x-0 md:space-y-2 grow">
         <NavLinks />
         <div className="md:block hidden bg-gray-50 rounded-md w-full h-auto grow"></div>
-        <form action={async ()=> {'use server'; await signOut(); }}>
+        <form action={async ()=> {
+          'use server'; 
+          await signOut();
+          }}
+        >
           <button className="flex md:flex-none justify-center md:justify-start items-center gap-2 bg-gray-50 hover:bg-sky-100 md:px-3 p-3 md:p-2 rounded-md w-full h-[48px] font-medium text-sm hover:text-blue-600 grow">
             <PowerIcon className="w-6" />
             <div className="md:block hidden">Sign Out</div>
